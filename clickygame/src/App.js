b36a123787850c07473dae503c88a6865ce762a7
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-28 19:08:30 -0700

    Added clas App extends Component function with state that includes cards,scores,high scores

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Card from './components/Card';
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import memorycards from "./memorycards.json"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
-}
+class App extends Component{
 
+  state={
+    cards,
+    currentScore : 0,
+    highScore: 0
+  };
+
+  
+
+
+  render(){
+    
+  }
+}
 export default App;
