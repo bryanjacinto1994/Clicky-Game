cbe56f0e7267f58f4ff1e3ba113ee432c3d3158f
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-28 20:23:11 -0700

    Updates

---
+++ precache-manifest.09b4e4d04d02eec6cfcbd68bb701e09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1884b31bbfad7a3c3b979546e53850",
+    "url": "/Clicky-Game/./index.html"
+  },
+  {
+    "revision": "00ac7e404bd4d2ee216b",
+    "url": "/Clicky-Game/./static/css/main.796c9b77.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/Clicky-Game/./static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "00ac7e404bd4d2ee216b",
+    "url": "/Clicky-Game/./static/js/main.e1b915f6.chunk.js"
+  },
+  {
+    "revision": "e56df06019b6b511cee3",
+    "url": "/Clicky-Game/./static/js/runtime-main.f6205249.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Clicky-Game/./static/media/logo.25bf045c.svg"
+  }
+]);
