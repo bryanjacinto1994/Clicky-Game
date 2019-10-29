f634886a04413612cdf66bf0f3282b11347db5bc
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-28 19:14:36 -0700

    Added a function of when the game ends, it will resets

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -14,7 +14,22 @@ class App extends Component{
     highScore: 0
   };
 
-  
+  endGame = () => {
+
+    if(this.state.currentScore > this.state.highScore) {
+      this.setState({highScore: this.state.currentScore}, function (){
+        console.log(this.state.highScore);
+      });
+    }
+
+    this.state.cards.forEach (card => {card.count = 0;});
+
+    alert(`End Game \n Your Score: ${this.state.currentScore}`);
+    this.setState({score: 0});
+    return true;
+  }
+
+
 
 
   render(){
